03dc7efc13b9d7e7f0c67149cc8eb5b85c30e5c5
Sandeep-456 <sandeepindugula456@gmail.com>
2025-07-19 15:50:22 +0530

    updated frontend url

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const storeOwnerRoutes = require("./routes/storeOwnerRoutes");
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'store-rating-app-xi.vercel.app'],
+  origin: ['http://localhost:3000', 'https://store-rating-app-xi.vercel.app/'],
   credentials: true,
 }));
 app.use(express.json());
