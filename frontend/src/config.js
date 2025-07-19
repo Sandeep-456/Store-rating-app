06406c346ab62652cf9c1a6856caa57e1b071612
Sandeep-456 <sandeepindugula456@gmail.com>
2025-07-19 15:43:37 +0530

    updated backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export const API_URL = isProd 
-  ? 'https://store-rating-app-backend-ka3t.onrender.com'
+  ? 'https://store-rating-app-backend-ka3t.onrender.com/api'
   : 'http://localhost:5000/api';
