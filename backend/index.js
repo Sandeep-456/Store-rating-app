b9b4e99bd0d3bfddc2d5c35f0ec436e4b0e1a79a
Sandeep-456 <sandeepindugula456@gmail.com>
2025-07-19 16:02:27 +0530

    changed the frontend url

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,10 @@ const storeOwnerRoutes = require("./routes/storeOwnerRoutes");
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://store-rating-app-xi.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://store-rating-app-xi.vercel.app'],
   credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
 }));
 app.use(express.json());
 
